bbc5e365a68eb858dbcca1330a86562107b1c978
Jake Liu <jakeliu444@gmail.com>
2026-04-13 10:49:59 -0400

    setup flask

--- app/__init__.py
+++ app/__init__.py
@@ -11,10 +11,29 @@ app.secret_key = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
 import auth
 app.register_blueprint(auth.bp)
 
-@app.get('/')
-def home_get():
-    return ("hi")
-#    return render_template("home.html")
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("auth/login.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template("auth/register.html")
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template("home.html")
+
+@app.route('/bar_chart', methods=['GET', 'POST'])
+def bar_chart():
+    return render_template("bar_chart.html")
+
+@app.route('/bollinger', methods=['GET', 'POST'])
+def bollinger():
+    return render_template("bollinger.html")
+
+@app.route('/line_chart', methods=['GET', 'POST'])
+def line_chart():
+    return render_template("line_chart.html")
 
 if __name__ == '__main__':
     app.debug = True
