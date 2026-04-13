887f87bce4e42f2bb3fc5486a8f85eb410145bda
Ivan Chen <ichen60@stuy.edu>
2026-04-13 11:09:35 -0400

    temp

--- app/build_db.py
+++ app/build_db.py
@@ -1,19 +1,27 @@
-#PLACEHOLDER FILE UNTIL WE HAVE MONGODB UP
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-import sqlite3
+uri = "mongodb://Cluster17576:ZFhsTF5SZmZa@ac-cm2kdg3-shard-00-00.b6r6yys.mongodb.net:27017,ac-cm2kdg3-shard-00-01.b6r6yys.mongodb.net:27017,ac-cm2kdg3-shard-00-02.b6r6yys.mongodb.net:27017/?ssl=true&replicaSet=atlas-tl4q5z-shard-0&authSource=admin&appName=Cluster17576"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
 
-DB_FILE = "data.db"
+database = client["data"]
+try:
+    database.drop_collection("profiles")
+finally: # always run regardless if dropped
+    database.create_collection("profiles")
+    database.profiles.insert_one({"name": "test1"})
+    database.profiles.insert_one({"name": "test2"})
 
-db = sqlite3.connect(DB_FILE)
-c = db.cursor()
-
-c.executescript("""
-    DROP TABLE IF EXISTS profiles;
-    CREATE TABLE profiles (
-        username TEXT PRIMARY KEY,
-        password TEXT
-    );
-""")
-
-db.commit()
-db.close()
+client.close()
+# # PLACEHOLDER UNTIL MONGO WORKS
+# c.executescript("""
+#     DROP TABLE IF EXISTS profiles;
+#     CREATE TABLE profiles (
+#         username TEXT PRIMARY KEY,
+#         password TEXT
+#     );
+# """)
+#
+# db.commit()
+# db.close()
