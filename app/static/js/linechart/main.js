5f128e644b2140848e73e4df48daa72c9b83cb72
Jake Liu <jakeliu444@gmail.com>
2026-04-16 10:41:00 -0400

    implemented bollinger and linechart

---
+++ app/static/js/linechart/main.js
@@ -0,0 +1,7 @@
+import { Runtime, Inspector } from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@4/dist/runtime.js";
+import define from "./line_chart.js";
+
+const runtime = new Runtime();
+runtime.module(define, name => {
+  if (name === "chart") return new Inspector(document.querySelector("#chart"));
+});
